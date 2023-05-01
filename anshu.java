9df055ff520ec9d5ef30af3a2dce41cc1e7a3325
anshudubey344 <95116627+anshudubey344@users.noreply.github.com>
2023-05-01 15:12:10 +0530

    Update anshu.java

--- anshu.java
+++ anshu.java
@@ -2,6 +2,7 @@ public class Main{
   public static void main (string[] args){
      System.out.println("Hello World");
     System.out.println("HI Hello");
+    System.out.println("HI Hello");
 
   }
 }
